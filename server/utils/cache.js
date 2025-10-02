1259e675a02129d515b51299791b044a1a8838a4
thaivanvinhdinh <dinhtvv.pnv@gmail.com>
2025-10-02 23:21:04 +0700

    add cache

--- server/utils/cache.js
+++ server/utils/cache.js
@@ -1,3 +1,3 @@
 const NodeCache = require('node-cache');
-const cache = new NodeCache({ stdTTL: 600 }); // cache 5 phút
+const cache = new NodeCache({ stdTTL: 600 }); // cache 10 phút
 module.exports = cache;
